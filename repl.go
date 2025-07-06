accec3f5e0ffb2574f7d0c180d50a512b5a0c18d
Daniel Bedggood <ddbedggood@gmail.com>
2025-07-06 17:56:15 +1200

    feat: enhance REPL with command handling and structured command definitions

--- repl.go
+++ repl.go
@@ -7,18 +7,49 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func startRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
+
+	commands := map[string]cliCommand{}
+
+	commands["help"] = cliCommand{
+		name:        "help",
+		description: "Displays a help message",
+		callback:    func() error { return commandHelp(commands) },
+	}
+	commands["exit"] = cliCommand{
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	}
 
 	for {
 		fmt.Print("Pokedex > ")
 
 		scanner.Scan()
 		userInput := scanner.Text()
+		words := cleanInput(userInput)
+		if len(words) == 0 {
+			continue
+		}
+		firstWord := words[0]
 
-		cleanedInput := cleanInput(userInput)
+		command, exists := commands[firstWord]
+		if !exists {
+			fmt.Println("Unknown command")
+			continue
+		}
 
-		fmt.Println("Your command was:", cleanedInput[0])
+		if err := command.callback(); err != nil {
+			fmt.Printf("Error executing command\n")
+			continue
+		}
 	}
 }
 
@@ -26,4 +57,18 @@ func cleanInput(text string) []string {
 	lowercaseText := strings.ToLower(text)
 	words := strings.Fields(lowercaseText)
 	return words
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp(commands map[string]cliCommand) error {
+	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n")
+	for _, command := range commands {
+		fmt.Println(command.name + ": " + command.description)
+	}
+	return nil
 }
