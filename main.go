a6c2e11289b37d1dab742be119c05032d3479a22
Daniel Bedggood <ddbedggood@gmail.com>
2025-07-06 16:54:04 +1200

    feat: implement command input handling in main function and update .gitignore

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		scanner.Scan()
+		userInput := scanner.Text()
+
+		cleanedInput := cleanInput(userInput)
+
+		fmt.Println("Your command was:", cleanedInput[0])
+	}
 }
 
 func cleanInput(text string) []string {
